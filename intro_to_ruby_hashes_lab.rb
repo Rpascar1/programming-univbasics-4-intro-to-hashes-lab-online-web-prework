80b2adab7a10230354b88dce359b47fa30e9a3a0
Richard Pascarelli <rpavesi84@gmail.com>
2019-08-16 17:50:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,10 +23,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash (hash,key)
-  if hash[]
-  hash[]+= 1
+  if hash[:key]
+  hash[:key]+= 1
   else
-  hash[] = 1
+  hash[:key] = 1
  end
 end
 
